1fdf5732124eadeee8ce3f9488eecb11914ed8b2
zhihaozhao <16700173@qq.com>
2025-08-15 15:44:26 +0800

    cpu bat modify

--- sweep_local.bat
+++ sweep_local.bat
@@ -40,7 +40,11 @@ for /L %%s in (0,1,9) do (
       --env_burst_rate 0.2 ^
       --label_noise_prob 0.1 ^
       --num_classes 8 ^
-      --ckpt_dir checkpoints\
+      --ckpt_dir checkpoints\ ^
+	  --amp ^
+	  --save_ckpt final ^
+      --val_every 3 ^
+      --num_workers_override 2
   )
 )
 
